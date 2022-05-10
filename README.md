641816e3d8eeef25271a8e46e0a52f8095c4fbf2
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-05-10 15:12:59 -0700

    added js, bio, style to project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # About-me-201d86
+
+## Lab 02
+
+## Author: Stephen Martinez
+
---
+++ js/app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// console.log('Hello World!');
+
+// let answerOne = prompt('Do I have Children?').toLowerCase();
+
+// //console.log(answerOne);
+
+// if(answerOne === 'yes' || answerOne === 'y'){
+//   alert('You are correct');
+// } else if(answerOne === 'no' || answerOne === 'n'){
+//   alert('Wrong, I have 6!');
+// }
+
+// let answerTwo = prompt('Do I love coding?').toLowerCase();
+
+// //console.log(answerTwo);
+
+// if(answerTwo === 'yes' || answerTwo === 'y'){
+//   alert('Yes, don\'t we all?');
+// }else if(answerTwo === 'no' || answerTwo === 'n'){
+//   alert('NO? Actually, I find it very exciting!');
+// }
+
+// let answerThree = prompt('Am I in my 40s?').toLowerCase();
+
+// //console.log(answerThree);
+
+// if(answerThree === 'yes' || answerThree === 'y'){
+//   alert('Wow, good guess!!');
+// }else if(answerThree === 'no' || answerThree === 'n'){
+//   alert('Thanks for that but no, I\'m 41');
+// }
+
+// let answerFour = prompt('Am I a Veteran?').toLowerCase();
+
+// //console.log(answerFour);
+
+// if(answerFour === 'yes' || answerFour === 'y'){
+//   alert('Correct, GO NAVY!');
+// }else if(answerFour === 'no' || answerFour === 'n'){
+//   alert('I am a Veteran of the US Navy!');
+// }
+
+// let answerFive = prompt('Do I live in WA?').toLowerCase();
+
+// //console.log(answerFive);
+
+// if(answerFive === 'yes' || answerFive === 'y'){
+//   alert('Correct, I live in Pierce County');
+// }else if(answerFive === 'no' || answerFive === 'n'){
+//   alert('Actually, I do.  I live in Pierce County, WA');
+// }
+
+// let userName = prompt('Thanks for playing, What is your name?');
+
+// //console.log(userName);
+
+// alert('Welcome to my page, ' + userName);
+
