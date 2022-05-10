6b20b32d6c574298b92c5b1bf24389f47f17c595
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-05-10 15:20:53 -0700

    removed to comment out slashes

--- js/app.js
+++ js/app.js
@@ -1,60 +1,60 @@
 'use strict';
 
-// console.log('Hello World!');
+console.log('Hello World!');
 
-// let answerOne = prompt('Do I have Children?').toLowerCase();
+let answerOne = prompt('Do I have Children?').toLowerCase();
 
-// //console.log(answerOne);
+//console.log(answerOne);
 
-// if(answerOne === 'yes' || answerOne === 'y'){
-//   alert('You are correct');
-// } else if(answerOne === 'no' || answerOne === 'n'){
-//   alert('Wrong, I have 6!');
-// }
+if(answerOne === 'yes' || answerOne === 'y'){
+  alert('You are correct');
+} else if(answerOne === 'no' || answerOne === 'n'){
+  alert('Wrong, I have 6!');
+}
 
-// let answerTwo = prompt('Do I love coding?').toLowerCase();
+let answerTwo = prompt('Do I love coding?').toLowerCase();
 
-// //console.log(answerTwo);
+//console.log(answerTwo);
 
-// if(answerTwo === 'yes' || answerTwo === 'y'){
-//   alert('Yes, don\'t we all?');
-// }else if(answerTwo === 'no' || answerTwo === 'n'){
-//   alert('NO? Actually, I find it very exciting!');
-// }
+if(answerTwo === 'yes' || answerTwo === 'y'){
+  alert('Yes, don\'t we all?');
+}else if(answerTwo === 'no' || answerTwo === 'n'){
+  alert('NO? Actually, I find it very exciting!');
+}
 
-// let answerThree = prompt('Am I in my 40s?').toLowerCase();
+let answerThree = prompt('Am I in my 40s?').toLowerCase();
 
-// //console.log(answerThree);
+//console.log(answerThree);
 
-// if(answerThree === 'yes' || answerThree === 'y'){
-//   alert('Wow, good guess!!');
-// }else if(answerThree === 'no' || answerThree === 'n'){
-//   alert('Thanks for that but no, I\'m 41');
-// }
+if(answerThree === 'yes' || answerThree === 'y'){
+  alert('Wow, good guess!!');
+}else if(answerThree === 'no' || answerThree === 'n'){
+  alert('Thanks for that but no, I\'m 41');
+}
 
-// let answerFour = prompt('Am I a Veteran?').toLowerCase();
+let answerFour = prompt('Am I a Veteran?').toLowerCase();
 
-// //console.log(answerFour);
+//console.log(answerFour);
 
-// if(answerFour === 'yes' || answerFour === 'y'){
-//   alert('Correct, GO NAVY!');
-// }else if(answerFour === 'no' || answerFour === 'n'){
-//   alert('I am a Veteran of the US Navy!');
-// }
+if(answerFour === 'yes' || answerFour === 'y'){
+  alert('Correct, GO NAVY!');
+}else if(answerFour === 'no' || answerFour === 'n'){
+  alert('I am a Veteran of the US Navy!');
+}
 
-// let answerFive = prompt('Do I live in WA?').toLowerCase();
+let answerFive = prompt('Do I live in WA?').toLowerCase();
 
-// //console.log(answerFive);
+//console.log(answerFive);
 
-// if(answerFive === 'yes' || answerFive === 'y'){
-//   alert('Correct, I live in Pierce County');
-// }else if(answerFive === 'no' || answerFive === 'n'){
-//   alert('Actually, I do.  I live in Pierce County, WA');
-// }
+if(answerFive === 'yes' || answerFive === 'y'){
+  alert('Correct, I live in Pierce County');
+}else if(answerFive === 'no' || answerFive === 'n'){
+  alert('Actually, I do.  I live in Pierce County, WA');
+}
 
-// let userName = prompt('Thanks for playing, What is your name?');
+let userName = prompt('Thanks for playing, What is your name?');
 
-// //console.log(userName);
+//console.log(userName);
 
-// alert('Welcome to my page, ' + userName);
+alert('Welcome to my page, ' + userName);
 
